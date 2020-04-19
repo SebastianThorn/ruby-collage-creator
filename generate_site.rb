498071741b79a9a562cc5306edfeb307543bd1da
Sebastian Thörn <thorn@p0int3r.se>
2020-04-19 16:42:25 +0000

    Add files

---
+++ generate_site.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+require "./class/photobatch.rb"
+
+#
+# Start new generation of images
+#FileUtils.rm(Dir.glob("out/images/*"))
+
+# Remove static files, keep the folders, we don't wan't to regen those.
+FileUtils.rm(Dir.glob("out/*").select {|f| File.file?(f)})
+
+puts
+pb = PhotoBatch.new("in")
+pb.newAlbum("urbanegoat_2020-04-19", "Urbane Goat, 2020-04-19")
+pb.newAlbum("photos", "Blandade Foton")
+pb.wrapper()
